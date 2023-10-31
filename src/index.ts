bbd9824b73baadd892766c44f047f9d170073c41
John Beresford <jberesford@volcaus.com>
2023-10-31 12:41:09 -0700

    add additionalModuleSources option
    - add option
    - update tests

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import { resolveGlobalName } from "./globalObjects";
 import { resolveTsType } from "./typescript";
 import { resolveWebApiPath } from "./webApi";
 import { resolveWebAudioName } from "./webaudio";
+import packageInfo from "../package.json";
 
 const version = Application.VERSION.split(/[\.-]/);
 const supportsObjectReturn = +version[1] > 23 || +version[2] >= 26;
@@ -18,6 +19,7 @@ const supportsObjectReturn = +version[1] > 23 || +version[2] >= 26;
 declare module "typedoc" {
     export interface TypeDocOptionMap {
         resolveUtilityTypes: boolean;
+        additionalModuleSources: string[];
     }
 }
 
@@ -29,6 +31,13 @@ export function load(app: Application) {
         defaultValue: true,
         help: "[typedoc-plugin-mdn-links]: Resolve references to Partial, Omit, etc to the TypeScript website.",
         type: ParameterType.Boolean,
+    });
+
+    app.options.addDeclaration({
+        name: "additionalModuleSources",
+        help: "[typedoc-plugin-mdn-links]: Additional module sources to resolve.",
+        type: ParameterType.Array,
+        defaultValue: [],
     });
 
     const resolvers = [
@@ -53,11 +62,15 @@ export function load(app: Application) {
     });
 
     app.converter.addUnknownSymbolResolver((declaration) => {
+        const validSources = [
+            ...app.options.getValue("additionalModuleSources"),
+            "typescript",
+        ];
+        const moduleSource = declaration.moduleSource;
+
         if (
-            declaration.moduleSource === "typescript" ||
-            declaration.moduleSource === "@webgpu/types" ||
-            (!declaration.moduleSource &&
-                declaration.resolutionStart === "global")
+            (moduleSource && validSources.includes(moduleSource)) ||
+            (!moduleSource && declaration.resolutionStart === "global")
         ) {
             const name = declaration.symbolReference?.path
                 ?.map((path) => path.path)
@@ -80,6 +93,19 @@ export function load(app: Application) {
             }
 
             return result;
+        } else if (
+            moduleSource &&
+            moduleSource !== "typedoc-plugin-mdn-links"
+        ) {
+            app.logger.info(
+                `[typedoc-plugin-mdn-links]: Skipping unexpected module source ${moduleSource}.`,
+            );
+            app.logger.info(
+                "If this is a valid module source, please add it to the --additionalModuleSources option.",
+            );
+            app.logger.info(
+                `If you think this module source should be supported by default, please open an issue at ${packageInfo.bugs.url}.`,
+            );
         }
     });
 }
--- src/test/plugin.test.ts
+++ src/test/plugin.test.ts
@@ -7,146 +7,161 @@ import {
     ReferenceType,
     TSConfigReader,
 } from "typedoc";
-import { test, expect, beforeAll } from "vitest";
+import { test, expect, beforeAll, describe } from "vitest";
 import { load } from "../index";
 
 let project: ProjectReflection;
 
-beforeAll(async () => {
-    const app = await Application.bootstrap({
-        entryPoints: ["src/testdata/links.ts"],
+describe("Statically defined names", () => {
+    beforeAll(async () => {
+        const app = await Application.bootstrap({
+            entryPoints: ["src/testdata/links.ts"],
+        });
+        load(app);
+
+        project = (await app.convert())!;
+        expect(project).toBeDefined();
     });
-    load(app);
 
-    project = (await app.convert())!;
-    expect(project).toBeDefined();
+    test("Handles canvas links", () => {
+        const refl = project.getChildByName("GLContext");
+        expect(refl).toBeInstanceOf(DeclarationReflection);
+        const type = (refl as DeclarationReflection).type;
+        expect(type).toBeInstanceOf(ReferenceType);
+
+        const ref = type as ReferenceType;
+        expect(ref.externalUrl).toBe(
+            "https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext",
+        );
+    });
+
+    test("Handles css links", () => {
+        const refl = project.getChildByName("AE");
+        expect(refl).toBeInstanceOf(DeclarationReflection);
+        const type = (refl as DeclarationReflection).type;
+        expect(type).toBeInstanceOf(ReferenceType);
+
+        const ref = type as ReferenceType;
+        expect(ref.externalUrl).toBe(
+            "https://developer.mozilla.org/en-US/docs/Web/API/AnimationEvent",
+        );
+    });
+
+    test("Handles dom links", () => {
+        const refl = project.getChildByName("Controller");
+        expect(refl).toBeInstanceOf(DeclarationReflection);
+        const type = (refl as DeclarationReflection).type;
+        expect(type).toBeInstanceOf(ReferenceType);
+
+        const ref = type as ReferenceType;
+        expect(ref.externalUrl).toBe(
+            "https://developer.mozilla.org/en-US/docs/Web/API/AbortController",
+        );
+    });
+
+    test("Handles globalObject links", () => {
+        const refl = project.getChildByName("Inf");
+        expect(refl).toBeInstanceOf(DeclarationReflection);
+        const type = (refl as DeclarationReflection).type;
+        expect(type instanceof QueryType).toBe(true);
+
+        const ref = type as QueryType;
+        expect(ref.queryType.externalUrl).toBe(
+            "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Infinity",
+        );
+    });
+
+    test("Handles scoped globalObject links", () => {
+        const refl = project.getChildByName("DTF");
+        expect(refl).toBeInstanceOf(DeclarationReflection);
+        const type = (refl as DeclarationReflection).type;
+        expect(type instanceof QueryType).toBe(true);
+
+        const ref = type as QueryType;
+        expect(ref.queryType.externalUrl).toBe(
+            "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat",
+        );
+    });
+
+    test("Handles web audio links", () => {
+        const refl = project.getChildByName("AContext");
+        expect(refl).toBeInstanceOf(DeclarationReflection);
+        const type = (refl as DeclarationReflection).type;
+        expect(type).toBeInstanceOf(ReferenceType);
+
+        const ref = type as ReferenceType;
+        expect(ref.externalUrl).toBe(
+            "https://developer.mozilla.org/en-US/docs/Web/API/AudioContext",
+        );
+    });
+
+    test("Handles comment links", () => {
+        const refl = project.getChildByName("comment");
+        expect(refl).toBeInstanceOf(DeclarationReflection);
+        const tags = refl?.comment?.summary.filter(
+            (f) => f.kind === "inline-tag",
+        ) as InlineTagDisplayPart[];
+
+        expect(tags).toEqual([
+            {
+                kind: "inline-tag",
+                tag: "@link",
+                target: "https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext",
+                text: "WebGLRenderingContext",
+            },
+            {
+                kind: "inline-tag",
+                tag: "@link",
+                target: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NaN",
+                text: "NaN",
+            },
+            {
+                kind: "inline-tag",
+                tag: "@link",
+                target: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat",
+                text: "Intl.DateTimeFormat",
+            },
+            {
+                kind: "inline-tag",
+                tag: "@link",
+                target: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat",
+                text: "custom",
+            },
+        ]);
+    });
 });
 
-test("Handles canvas links", () => {
-    const refl = project.getChildByName("GLContext");
-    expect(refl).toBeInstanceOf(DeclarationReflection);
-    const type = (refl as DeclarationReflection).type;
-    expect(type).toBeInstanceOf(ReferenceType);
+describe("Fallback to webApi index", () => {
+    beforeAll(async () => {
+        const app = await Application.bootstrap({
+            entryPoints: ["src/testdata/links.ts"],
+        });
+        load(app);
+        app.options.setValue("additionalModuleSources", ["@webgpu/types"]);
 
-    const ref = type as ReferenceType;
-    expect(ref.externalUrl).toBe(
-        "https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext",
-    );
-});
+        project = (await app.convert())!;
+        expect(project).toBeDefined();
+    });
 
-test("Handles css links", () => {
-    const refl = project.getChildByName("AE");
-    expect(refl).toBeInstanceOf(DeclarationReflection);
-    const type = (refl as DeclarationReflection).type;
-    expect(type).toBeInstanceOf(ReferenceType);
+    test("Handles otherwise unknown links", () => {
+        const refl = project.getChildByName("WGpuDevice");
+        expect(refl).toBeInstanceOf(DeclarationReflection);
+        const type = (refl as DeclarationReflection).type;
+        expect(type).toBeInstanceOf(ReferenceType);
 
-    const ref = type as ReferenceType;
-    expect(ref.externalUrl).toBe(
-        "https://developer.mozilla.org/en-US/docs/Web/API/AnimationEvent",
-    );
-});
+        const ref = type as ReferenceType;
+        expect(ref.externalUrl).toBe(
+            "https://developer.mozilla.org/en-US/docs/Web/API/GPUDevice",
+        );
+    });
 
-test("Handles dom links", () => {
-    const refl = project.getChildByName("Controller");
-    expect(refl).toBeInstanceOf(DeclarationReflection);
-    const type = (refl as DeclarationReflection).type;
-    expect(type).toBeInstanceOf(ReferenceType);
+    test("Does not handle non-existent links", () => {
+        const refl = project.getChildByName("NotOnMdn");
+        expect(refl).toBeInstanceOf(DeclarationReflection);
+        const type = (refl as DeclarationReflection).type;
+        expect(type).toBeInstanceOf(ReferenceType);
 
-    const ref = type as ReferenceType;
-    expect(ref.externalUrl).toBe(
-        "https://developer.mozilla.org/en-US/docs/Web/API/AbortController",
-    );
-});
-
-test("Handles globalObject links", () => {
-    const refl = project.getChildByName("Inf");
-    expect(refl).toBeInstanceOf(DeclarationReflection);
-    const type = (refl as DeclarationReflection).type;
-    expect(type instanceof QueryType).toBe(true);
-
-    const ref = type as QueryType;
-    expect(ref.queryType.externalUrl).toBe(
-        "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Infinity",
-    );
-});
-
-test("Handles scoped globalObject links", () => {
-    const refl = project.getChildByName("DTF");
-    expect(refl).toBeInstanceOf(DeclarationReflection);
-    const type = (refl as DeclarationReflection).type;
-    expect(type instanceof QueryType).toBe(true);
-
-    const ref = type as QueryType;
-    expect(ref.queryType.externalUrl).toBe(
-        "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat",
-    );
-});
-
-test("Handles web audio links", () => {
-    const refl = project.getChildByName("AContext");
-    expect(refl).toBeInstanceOf(DeclarationReflection);
-    const type = (refl as DeclarationReflection).type;
-    expect(type).toBeInstanceOf(ReferenceType);
-
-    const ref = type as ReferenceType;
-    expect(ref.externalUrl).toBe(
-        "https://developer.mozilla.org/en-US/docs/Web/API/AudioContext",
-    );
-});
-
-test("Handles comment links", () => {
-    const refl = project.getChildByName("comment");
-    expect(refl).toBeInstanceOf(DeclarationReflection);
-    const tags = refl?.comment?.summary.filter(
-        (f) => f.kind === "inline-tag",
-    ) as InlineTagDisplayPart[];
-
-    expect(tags).toEqual([
-        {
-            kind: "inline-tag",
-            tag: "@link",
-            target: "https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext",
-            text: "WebGLRenderingContext",
-        },
-        {
-            kind: "inline-tag",
-            tag: "@link",
-            target: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NaN",
-            text: "NaN",
-        },
-        {
-            kind: "inline-tag",
-            tag: "@link",
-            target: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat",
-            text: "Intl.DateTimeFormat",
-        },
-        {
-            kind: "inline-tag",
-            tag: "@link",
-            target: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat",
-            text: "custom",
-        },
-    ]);
-});
-
-test("Handles otherwise unknown links", () => {
-    const refl = project.getChildByName("WGpuDevice");
-    expect(refl).toBeInstanceOf(DeclarationReflection);
-    const type = (refl as DeclarationReflection).type;
-    expect(type).toBeInstanceOf(ReferenceType);
-
-    const ref = type as ReferenceType;
-    expect(ref.externalUrl).toBe(
-        "https://developer.mozilla.org/en-US/docs/Web/API/GPUDevice",
-    );
-});
-
-test("Does not handle non-existent links", () => {
-    const refl = project.getChildByName("NotOnMdn");
-    expect(refl).toBeInstanceOf(DeclarationReflection);
-    const type = (refl as DeclarationReflection).type;
-    expect(type).toBeInstanceOf(ReferenceType);
-
-    const ref = type as ReferenceType;
-    expect(ref.externalUrl).toBeUndefined();
+        const ref = type as ReferenceType;
+        expect(ref.externalUrl).toBeUndefined();
+    });
 });
