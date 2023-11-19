9a4959ebe3acb463e6e0d2e55a59f53b6cad1878
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2023-11-19 07:26:57 -0700

    Include browser-compat-data version in changelog

--- scripts/commit_web_api_changes.sh
+++ scripts/commit_web_api_changes.sh
@@ -5,12 +5,13 @@ set -e
 if ! git diff --exit-code data/web-api.json >/dev/null; then
     echo "Web API index changed, building new release"
     version=$(npm version patch --git-tag-version=false)
+    compat_version=$(node -p "JSON.parse(fs.readFileSync('node_modules/@mdn/browser-compat-data/package.json')).version")
 
 extra=$"\
 \n\
 ### $version ($(date '+%Y-%m-%d'))\n\
 \n\
--   Updated MDN API index\n\
+-   Updated MDN API index with @mdn/browser-compat-data version $compat_version\n\
 "
 
     awk $"
